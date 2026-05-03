7bf70f18991b9d7943ba24fed06c3f88e75daecb
Janvi Maggu <janvi.cs27@iilm.edu>
2026-05-04 04:07:52 +0530

    fix frontend route

--- resume-ai/backend/server.js
+++ resume-ai/backend/server.js
@@ -120,7 +120,15 @@ jdWords.forEach(word => {
     res.status(500).json({ error: err.message });
   }
 });
+const path = require("path");
 
+// 👇 frontend serve
+app.use(express.static(path.join(__dirname, "..")));
+
+// 👇 default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
